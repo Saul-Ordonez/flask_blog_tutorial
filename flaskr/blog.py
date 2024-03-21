2de7357cb81544efaa3f49f663824c1485a92500
Saul Ordonez <saul_ordonez07@comcast.net>
2024-03-21 12:02:37 -0700

    create delete view and update update.html template with delete button. Also fixes minor typo bugs preventing the user from access the correct URL path.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -83,4 +83,15 @@ def update(id):
         (title, body, id)
       )
       db.commit()
-      return render_template('blog/update.html', post=post)
+      return redirect(url_for('blog.index'))
+    
+  return render_template('blog/update.html', post=post)
+    
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+  get_post(id)
+  db = get_db()
+  db.execute('DELETE FROM post WHERE id = ?', (id,))
+  db.commit()
+  return redirect(url_for('blog.index'))
